660e00ce35be82791eeac3b8b550300900fd0cad
duwaar <duwaar@yahoo.com>
2019-09-01 13:38:26 -0500

    Refactoring

--- braille_keyboard.py
+++ braille_keyboard.py
@@ -12,34 +12,41 @@ import pyglet
 
 class BrailleApp(pyglet.window.Window):
     def __init__(self):
-        super().__init__(width=640, height=480)
+        # Create a window with the caption "Braille Notepad".
+        super().__init__(
+                width=640, height=480,
+                caption='\u2820\u2803\u2817\u2801\u280a\u2807\u2807\u2811'\
+                        + '\u2800\u2820\u281d\u2815\u281e\u2811\u280f\u2801\u2819')
 
         self.batch = pyglet.graphics.Batch()
         self.background = pyglet.graphics.OrderedGroup(0)
         self.foreground = pyglet.graphics.OrderedGroup(1)
 
         self.unicode_offset = int('0x2800', 16) # Six-dot Braille characters start at U+2800
-        self.braille_keys = {102:1,
-                             100:2,
-                             115:3,
-                             106:4,
-                             107:5,
-                             108:6,
-                             97:'left',
-                             59:'right',
-                             32:'space'}
-        self.current_cell = {1:False,
-                             2:False,
-                             3:False,
-                             4:False,
-                             5:False,
-                             6:False}
+        self.braille_keys = {
+                102:1,
+                100:2,
+                115:3,
+                106:4,
+                107:5,
+                108:6,
+                97:'left',
+                59:'right',
+                32:'space'
+                }
+        self.current_cell = {
+                1:False,
+                2:False,
+                3:False,
+                4:False,
+                5:False,
+                6:False
+                }
         self.key_buffer = []
         self.special_keys = []
         self.cursor = [0, 0]
         self.blank_line = [chr(0 + self.unicode_offset)] * 40 + ['\n']
-        self.line = self.blank_line.copy()
-        self.document = []
+        self.document = [self.blank_line.copy()] # Start document with one blank line.
 
     def get_cell_value(self):
         value = 0
@@ -69,7 +76,7 @@ class BrailleApp(pyglet.window.Window):
             value = self.get_cell_value()
             if value > 0:
                 #print(chr(value + self.unicode_offset))
-                self.line[self.cursor[0]] = chr(value + self.unicode_offset)
+                self.document[self.cursor[1]][self.cursor[0]] = chr(value + self.unicode_offset)
                 self.cursor[0] += 1
                 for dot in self.current_cell:
                     self.current_cell[dot] = False
@@ -77,7 +84,7 @@ class BrailleApp(pyglet.window.Window):
             else:
                 for key in self.special_keys:
                     if key == 'space':
-                        self.line[self.cursor[0]] = chr(0 + self.unicode_offset)
+                        self.document[self.cursor[1]][self.cursor[0]] = chr(0 + self.unicode_offset)
                         self.cursor[0] += 1
                     elif key == 'right':
                         self.cursor[0] += 1
@@ -87,13 +94,14 @@ class BrailleApp(pyglet.window.Window):
 
             # Change lines if we go off the end of the current one.
             if self.cursor[0] > 40:
-                self.document.append(self.line)
                 self.cursor[1] += 1
-                self.line = self.blank_line.copy()
                 self.cursor[0] = 0
+                # Add a new line if necessary.
+                if self.cursor[1] > len(self.document) - 1:
+                    self.document.append(self.blank_line.copy())
             elif self.cursor[0] < 0:
-                self.cursor[0] = 40
                 self.cursor[1] -= 1
+                self.cursor[0] = 40
 
         #print(self.cursor)
 
@@ -108,15 +116,14 @@ class BrailleApp(pyglet.window.Window):
                          width, 0)),
                 ('c3B', (240,) * 12))
 
-
         # Draw Braille document text.
         display_text = ''
-        for line in self.document + [self.line]:
+        for line in self.document:
             for char in line:
                 display_text += char
             display_text += '\n'
         pyglet.text.Label(text=display_text,
-                          font_size=12,
+                          font_size=18,
                           color=(0, 0, 0, 255),
                           x=0, y=height,
                           width=width,
@@ -128,7 +135,8 @@ class BrailleApp(pyglet.window.Window):
                           group=self.foreground)
 
         # Draw status bar.
-        bar = "cursor: " + str(self.cursor)
+        bar = 'cursor: ' + str(self.cursor)\
+                + ' | lines: ' + str(len(self.document))
         pyglet.text.Label(text=bar,
                           font_size=12,
                           multiline=False,
